3278efc8e207bf99e183ab64ec628a93e29f4216
salnyed <agorbuncova2004@gmail.com>
2023-05-28 19:23:43 +0400

    Added save asymmetric keys

--- workWIthFiles.py
+++ workWIthFiles.py
@@ -21,3 +21,29 @@ def loadSettings(settingsFile: str) -> dict:
     except OSError as err:
         logging.warning(f' Error when reading settings from a file {settingsFile}\n{err}')
     return settings
+
+
+def saveAsymmetricKeys(privateKey, publicKey, privatePem: str, publicPem: str) -> None:
+    """
+    The function stores the private and public key for asymmetric encryption
+    :arg privateKey: private key
+    :arg publicKey: public key
+    :arg privatePem: the name of the private key file
+    :arg publicPem: the name of the public key file
+    :return: None
+    """
+    try:
+        with open(privatePem, 'wb') as privateOut:
+            privateOut.write(privateKey.private_bytes(encoding=serialization.Encoding.PEM,
+                                                      format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                      encryption_algorithm=serialization.NoEncryption()))
+        logging.info(f' The private key was successfully saved to a file {privatePem}')
+    except OSError as err:
+        logging.warning(f' Error when saving private key to a file {privatePem}\n{err}')
+    try:
+        with open(publicPem, 'wb') as public_out:
+            public_out.write(publicKey.public_bytes(encoding=serialization.Encoding.PEM,
+                                                    format=serialization.PublicFormat.SubjectPublicKeyInfo))
+        logging.info(f' The public key was successfully saved to a file {publicPem}')
+    except OSError as err:
+        logging.warning(f' Error when saving public key to a file {publicPem}\n{err}')
