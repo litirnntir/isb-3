760716779fc1359989ab55b234242e88fa6c521c
salnyed <agorbuncova2004@gmail.com>
2023-05-28 19:11:36 +0400

    Added load settings file

--- workWIthFiles.py
+++ workWIthFiles.py
@@ -5,3 +5,19 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
+
+
+def loadSettings(settingsFile: str) -> dict:
+    """
+    The function reads the settings file
+    :arg settingsFile: name of the settings file
+    :return: settings
+    """
+    settings = None
+    try:
+        with open(settingsFile) as jsonFile:
+            settings = json.load(jsonFile)
+        logging.info(f' Settings read from file {settingsFile}')
+    except OSError as err:
+        logging.warning(f' Error when reading settings from a file {settingsFile}\n{err}')
+    return settings
