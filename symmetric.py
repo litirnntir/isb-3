1c67f315af5aad3507b16a72e17c15e16850dfe3
salnyed <agorbuncova2004@gmail.com>
2023-05-28 18:25:26 +0400

    Added encrypt for symmetric

--- symmetric.py
+++ symmetric.py
@@ -22,3 +22,26 @@ def generateSymmetricKey(length: int) -> str:
         logging.info(
             ' The length of the key is not equal to 128, 192, 256')
     return key
+
+
+def encryptSymmetric(key: bytes, text: bytes, length: int) -> bytes:
+    """
+    The function encrypts the text with the Camellia symmetric encryption algorithm
+    :arg length: the length of the key
+    :arg text: the text to be encrypted
+    :arg key: key
+    :return: the encrypted text
+    """
+    iv = 0
+    cipherText = 0
+    try:
+        padder = padding.ANSIX923(length).padder()
+        paddedText = padder.update(text) + padder.finalize()
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.Camellia(key), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        cipherText = encryptor.update(paddedText) + encryptor.finalize()
+        logging.info(f' The text is encrypted with the Camellia symmetric encryption algorithm')
+    except OSError as err:
+        logging.warning(f' Symmetric encryption error {err}')
+    return iv + cipherText
